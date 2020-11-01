f75e7d7c6803147672d4678a66dbd026b05eaba1
Alan Liddell <epiphyte2@protonmail.com>
2020-11-01 15:25:39 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='teleologic-api',
+    version='0.1.0',
+    packages=['teleologic'],
+    url='https://teleologic.io',
+    license='MIT',
+    author='Alan Liddell',
+    author_email='epiphyte2@protonmail.com',
+    description=''
+)
